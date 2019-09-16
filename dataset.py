c5df4b1b2e2313443d0c810667032fb010c010f0
qiao1406 <lwt1406@buaa.edu.cn>
2019-09-16 12:12:38 +0800

    train.py

--- dataset.py
+++ dataset.py
@@ -2,6 +2,15 @@ import h5py
 import torch
 
 
+class TrainingInstance:
+
+    def __init__(self, x, lc, lp, lq, p, q, t):
+        self.s_c = x[t - lc: t].reshape(lc * 2, 20, 20)
+        self.s_p = x[t - lp * p: t: p].reshape(lp * 2, 20, 20)
+        self.s_q = x[t - lq * q: t: q].reshape(lq * 2, 20, 20)
+        self.x_t = x[t]
+
+
 def load_dataset(filename):
     f = h5py.File(filename, 'r')
     data = torch.from_numpy(f['data'].value)
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-import torch.nn.functional as F
+import torch
+import torch.nn.functional as func
 from torch import nn
 
 
@@ -15,17 +16,17 @@ class ResUnit(nn.Module):
 
     # x channel num: 64
     def forward(self, x):
-        f = self.conv1(F.relu(x))
-        f = self.conv2(F.relu(f))
+        f = self.conv1(func.relu(x))
+        f = self.conv2(func.relu(f))
         x = x + f
 
         return x
 
 
-class SharedComponent(nn.Module):
+class STResNet(nn.Module):
 
     def __init__(self, res_layers, interval):
-        super(SharedComponent, self).__init__()
+        super(STResNet, self).__init__()
         self.res_layers = res_layers
 
         self.conv1 = nn.Conv2d(2 * interval, 64, 3)
@@ -39,9 +40,19 @@ class SharedComponent(nn.Module):
 
         # L layers ResUnit
         for l in range(self.res_layers):
-            x = self.res_units[l]
+            x = self.res_units[l](x)
 
         # conv layer 2
         x = self.conv2(x)
 
         return x
+
+
+class RMSELoss(nn.Module):
+
+    def __init__(self):
+        super(RMSELoss, self).__init__()
+        self.mse = nn.MSELoss()
+
+    def forward(self, y_hat, y):
+        return torch.sqrt(self.mse(y_hat, y))
